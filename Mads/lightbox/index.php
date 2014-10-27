d3d8cc7291e62479b9698019e77b6c8e4c571180
Mads Skaaning <mads.skaaning@gmail.com>
2014-10-27 14:11:29 +0100

    Twitter Bootstrap framework + fontawesome

---
+++ Mads/lightbox/index.php
@@ -0,0 +1,33 @@
+<php 
+
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>CBS Calendar Extra</title>
+	
+	
+	<!-- Importing FontAwe !-->
+	<link rel="stylesheet" href="font-awesome/css/font-awesome.min.css"> 	
+	
+	<!-- Using this reset.css to reset styles. !-->
+        <link rel="stylesheet" href="css/reset.css" type="text/css" media="screen" />
+	<!-- Personal reference to style for CBS Calender Extra !-->
+        <link rel="stylesheet" href="css/style.css" type="text/css" media="screen" />
+	<!-- Loading extre defined MyStyles !-->
+	<link rel="stylesheet" href="css/mystyle.css" type="text/css" media="screen" />
+	<!-- Below to </head> is just for testing purposes.... !-->
+</head>
+<title>CBS Calendar login</title>
+<body>
+	
+
+</body>
+
+
+</html>
+
+
+
+
+?>
---
+++ Mads/subpage.php
@@ -0,0 +1,179 @@
+<php 
+
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>CBS Calendar Extra</title>
+	
+	
+	<!-- Importing FontAwe !-->
+	<link rel="stylesheet" href="font-awesome/css/font-awesome.min.css"> 	
+	
+	<!-- Using this reset.css to reset styles. !-->
+        <link rel="stylesheet" href="css/reset.css" type="text/css" media="screen" />
+	<!-- Personal reference to style for CBS Calender Extra !-->
+        <link rel="stylesheet" href="css/style.css" type="text/css" media="screen" />
+	<!-- Loading extre defined MyStyles !-->
+	<link rel="stylesheet" href="css/mystyle.css" type="text/css" media="screen" />
+	<!-- Below to </head> is just for testing purposes.... !-->
+	        <link href="lightbox/css/lightbox.css" rel="stylesheet" />
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans|Baumans' rel='stylesheet' type='text/css'>
+        
+        <script src="js/vendor/modernizr.min.js"></script>
+        <script src="js/vendor/respond.min.js"></script>
+        <!-- include extern jQuery file but fall back to local file if extern one fails to load !-->
+        <script src="http://code.jquery.com/jquery-1.7.2.min.js"></script>
+        <script type="text/javascript">window.jQuery || document.write('<script type="text/javascript" src="js\/vendor\/1.7.2.jquery.min"><\/script>')</script>
+        
+        <script src="lightbox/js/lightbox.js"></script>
+        <script src="js/vendor/prefixfree.min.js"></script>
+        <script src="js/vendor/jquery.slides.min.js"></script>
+        <script src="js/script.js"></script>
+        
+	
+	</head>
+
+	<body>
+        <header>
+            <div class="toggleMobile">
+                <span class="menu1"></span>
+                <span class="menu2"></span>
+                <span class="menu3"></span>
+            </div>
+            <div id="mobileMenu">
+                <ul>
+                    <li><a href="javascript:void(0)"><a class="list-group-item" href="#"><i class="fa fa-home fa-fw"></i>Home</a></li>
+                    <li><a href="javascript:void(0)">Porfolio</a></li>
+                    <li><a href="javascript:void(0)">About</a></li>
+                </ul>
+            </div>           
+            <h1>CBS Calendar</h1>
+            <p>Extra</p>
+	     <div class="list-group">
+</div>
+            
+            <nav>
+            	<h2 class="hidden">Our navigation</h2>
+                <ul>
+                    <li><a href="javascript:void(0)">1</a></li>
+                    <li><a href="javascript:void(0)">2</a></li>
+                    <li><a href="javascript:void(0)">3</a></li>
+                    <li><a href="javascript:void(0)">4</a></li>
+                </ul>
+            </nav>
+        </header>
+        <section class="container">
+        </section>
+        <section id="spacer">  
+        	<h2 class="hidden">Hidden.</h2>          
+            <p>CBS Calendar</p>
+            <div class="search">
+                <form action="#">
+                    <input type="text" name="sitesearch" value="Enter a word..."/>
+                    <input type="submit" name="start_search" class="button" value="Search"/>
+                </form>
+            </div>            
+        </section>
+        <section id="boxcontent">
+            <article>            	                           
+                <h3>Slider her.</h3>
+                <p>        	
+                    Her kunne slideren eventuelt være. Med reklame. Højresiden til loginboksen.
+                </p>
+            </article>
+            <article>            	     
+                <h3>Log-in</h3>
+                <p>        	
+		<div class="input-group margin-bottom-sm">
+	<span class="input-group-addon"><i class="fa fa-envelope-o fa-fw"></i></span>
+	 <input class="form-control" type="text" placeholder="Email address">
+	</div>
+	<div class="input-group">
+	 <span class="input-group-addon"><i class="fa fa-key fa-fw"></i></span>
+	<input class="form-control" type="password" placeholder="Password">
+	    </div>
+                </p>
+            </article>
+            <br class="clear"/>
+        </section>
+        
+        <section id="four_columns">
+            <h2>
+                ...
+            </h2>    
+            <article class="img-item">
+            	<h3 class="hidden">Dolor sit amet</h3>
+                <figure>  
+                    <a href="google.dk" rel="lightbox" title="Some title">
+                        <span class="thumb-screen"></span>
+                    </a>
+		    
+                    <figcaption>
+                        <strong>
+			    <i class="fa fa-camera-retro fa-4x"></i>
+                        </strong>
+                      <p>  Ingen fotogenisk hukommelse?</p>
+                    </figcaption>
+		    
+                </figure>
+            </article>
+            <article class="img-item">
+            	<h3 class="hidden">Sit Amet</h3>
+                <figure>
+					
+                        <span class="thumb-screen"></span>
+                    </a>
+                                       <figcaption>
+					<strong>
+                            <i class="fa  fa-chain fa-4x"></i>
+			    </strong>
+                        <p>CBS Calendar Extra er det lette bindeled mellem CBS Calendar og Learn.</p>
+			
+                    </figcaption>
+                </figure>
+            </article>
+            <article class="img-item">
+            	<h3 class="hidden">Dolor Sit</h3>
+                <figure>
+                        <span class="thumb-screen"></span>
+                    </a>
+		      <figcaption class="">
+                    <figcaption>
+                        <strong>
+			    <i class="fa fa-bank fa-4x"></i>
+                        </strong>
+                        Tid er penge.
+						- Spar tid på at finde undervisningenplanen på learn hver gang.
+                    </figcaption>
+                </figure>
+            </article>
+            <article class="img-item">
+            	<h3 class="hidden">Lorem Ipsum</h3>
+                <figure>
+                        <span class="thumb-screen"></span>
+                    </a>
+                    <figcaption>
+                        <strong>
+                            <i class="fa fa-android fa-5x"></i>
+                        </strong>
+						<p>
+                        Eventuelt ekstra tekst her.
+						</p>
+                    </figcaption>
+		    
+                </figure>
+            </article>
+            <br class="clear"/>
+        </section>
+        <footer class="copyrightfield">
+            <section id="copyright">
+                <div class="wrapper">
+                    &copy; Copyright 2014 by <a href="http://www.calendar.cbs.dk">CBS Calendar Extra</a>. All Rights Reserved.
+					<p align="center">Read more in <a href="#">disclaimer</a></p>
+                </div>
+            </section>
+        </footer>
+	</body>
+</html>
+ ?>
