5123dc837f34ecd82753cbd8b05a30a547f32d6d
Mads Skaaning <mads.skaaning@gmail.com>
2014-11-10 13:35:35 +0100

    ss
    ss

--- Serverapplikation/src/config/RunMe.java
+++ Serverapplikation/src/config/RunMe.java
@@ -9,7 +9,7 @@ public class RunMe {
 			System.out.println("IP Address is " + cf.getHost());
 			System.out.println("Weather longitude is: " + cf.getWeather_lon());
 			System.out.println("Weather lat is: " + cf.getWeather_lat());
-			System.out.println("Weather is set to display: " + cf.getWeather_future_in_days() + "days from today");
+			System.out.println("Weather is set to display: " + cf.getWeather_future_in_days() + " days from today");
 			System.out.println("" + cf.getFfcryptkey());
 	}
 }
