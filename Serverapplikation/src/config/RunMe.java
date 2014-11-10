f21a0ae3bf819087a5be23eec575234b6ae3e491
Hasse Walin <hassewalin@hotmail.com>
2014-11-10 14:33:25 +0100

    Tilføjet Print af Password

--- Serverapplikation/src/config/RunMe.java
+++ Serverapplikation/src/config/RunMe.java
@@ -10,6 +10,7 @@ public class RunMe {
 			System.out.println("Weather longitude is: " + cf.getWeather_lon());
 			System.out.println("Weather lat is: " + cf.getWeather_lat());
 			System.out.println("Weather is set to display: " + cf.getWeather_future_in_days() + " days from today");
+			System.out.println("PASS: " + cf.getPassword());
 			System.out.println("" + cf.getFfcryptkey());
 	}
 }
