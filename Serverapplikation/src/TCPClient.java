f0c86273f249725aaad4cf06583325f9c6fc85da
faurholdt <philip.faurholdt@gmail.com>
2014-11-17 14:10:59 +0100

    IP -> localhost

--- Serverapplikation/src/TCPClient.java
+++ Serverapplikation/src/TCPClient.java
@@ -36,7 +36,7 @@ public class TCPClient {
 		Configurations cf = new Configurations();
 		
 		
-		Socket clientSocket = new Socket("172.17.143.102", 8888);
+		Socket clientSocket = new Socket("localhost", 8888);
 		DataOutputStream outToServer = new DataOutputStream(
 				clientSocket.getOutputStream());
 		byte[] input = gsonString.getBytes();
