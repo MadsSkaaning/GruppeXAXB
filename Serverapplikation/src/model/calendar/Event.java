e09273279ca7f935b95e7a2e77b97e24229dc9c7
Hasse Walin <hassewalin@hotmail.com>
2014-12-05 22:04:23 +0100

    Event lavet

--- Serverapplikation/src/model/calendar/Event.java
+++ Serverapplikation/src/model/calendar/Event.java
@@ -3,108 +3,104 @@ package model.calendar;
 import java.util.ArrayList;
 
 /**
- * Created by jesperbruun on 10/10/14.
- * Til hver specifik event bliver de defineret saaledes.
+ * Created by jesperbruun on 10/10/14. Til hver specifik event bliver de
+ * defineret saaledes.
  */
 public class Event {
-    private String eventid;
-    private String description;
-    private String location;
-    private String createdby;
-    private ArrayList<String> start;
-    private ArrayList<String> end;
-    private String calendarId;
-    private String active;
-    private String eventName;
-    private String customEvent;
-    
-    
+	private String activityid;
+	private String eventid;
+	private String type;
+	private String title;
+	private String description;
+	private String location;
+	private String createdby;
+	private ArrayList<String> start;
+	private ArrayList<String> end;
 
-    // Settere og gettere for Event objektet 
+	// Settere og gettere for Event objektet
+	public void setActivityid(String activityid) {
+		this.activityid = activityid;
+	}
 
-    public void setEventid(String eventid){
-        this.eventid = eventid;
-    }
-    public String getEventid(){
-        return eventid;
-    }
+	public String getActivityid() {
+		return activityid;
+	}
 
-
-    public void setDescription(String description){
-        this.description = description;
-    }
-    public String getDescription(){
-        return description;
-    }
-
-    public void setLocation(String location){
-        this.location = location;
-    }
-    public String getLocation(){
-        return location;
-    }
-
-    public void setCreatedby(String createdby){
-        this.createdby = createdby;
-    }
-    public String getCreatedby(){
-        return createdby;
-    }
-    
-    public void setStart(ArrayList<String> start){
-        this.start = start;
-    }
-    public ArrayList<String> getStart(){
-        return start;
-    }
-
-    public void setEnd(ArrayList<String> end){
-        this.end = end;
-    }
-    public ArrayList<String> getEnd(){
-        return end;
-    }
-	public Event(String eventid, String eventName,
-			String description, String location, String createdby,
-			 String calendarId, String active, String customEvent, ArrayList<String> start, ArrayList<String> end) {
-		super();
+	public void setEventid(String eventid) {
 		this.eventid = eventid;
-		this.eventName = eventName;
+	}
+
+	public String getEventid() {
+		return eventid;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setCreatedby(String createdby) {
+		this.createdby = createdby;
+	}
+
+	public String getCreatedby() {
+		return createdby;
+	}
+
+	public void setStart(ArrayList<String> start) {
+		this.start = start;
+	}
+
+	public ArrayList<String> getStart() {
+		return start;
+	}
+
+	public void setEnd(ArrayList<String> end) {
+		this.end = end;
+	}
+
+	public ArrayList<String> getEnd() {
+		return end;
+	}
+
+	public Event(String activityid, String eventid, String type, String title,
+			String description,
+			ArrayList<String> start, ArrayList<String> end, String location) {
+		super();
+		this.activityid = activityid;
+		this.eventid = eventid;
+		this.type = type;
+		this.title = title;
 		this.description = description;
 		this.location = location;
-		this.createdby = createdby;
 		this.start = start;
 		this.end = end;
-		this.calendarId = calendarId;
-		this.active = active;
-		this.customEvent = customEvent;
-		
 	}
-	public String getCalendarId() {
-		return calendarId;
-	}
-	public void setCalendarId(String calendarId) {
-		this.calendarId = calendarId;
-	}
-	public String getActive() {
-		return active;
-	}
-	public void setActive(String active) {
-		this.active = active;
-	}
-	public String getEventName() {
-		return eventName;
-	}
-	public void setEventName(String eventName) {
-		this.eventName = eventName;
-	}
-	public String getCustomEvent() {
-		return customEvent;
-	}
-	public void setCustomEvent(String customEvent) {
-		this.customEvent = customEvent;
-	}
-    
-    
-
 }
