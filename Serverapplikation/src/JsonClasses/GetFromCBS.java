b82586bd8f02b8b53e097a2fe6976699753e2216
Hasse Walin <hassewalin@hotmail.com>
2014-12-06 22:17:52 +0100

    havde glemt vigtig element, NU VIRKER DET
    (Med quick fix stadig dog).....

---
+++ Serverapplikation/src/JsonClasses/GetFromCBS.java
@@ -0,0 +1,15 @@
+package JsonClasses;
+
+public class GetFromCBS {
+	
+	private  final long serialVersionUID = 1L;
+	private String overallID = "importCalendar";
+	
+	public String getOverallID() {
+		return overallID;
+	}
+	public void setOverallID(String overallID) {
+		this.overallID = overallID;
+
+	}
+}
